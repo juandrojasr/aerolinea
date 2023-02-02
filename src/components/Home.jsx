3ae5e84c90ec52e70f2fabfc1a2955eb9fecd3bf
juandrojasr <juandrojasr97@gmail.com>
2023-02-02 17:22:47 -0500

    feat: home

---
+++ src/components/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+    <div className='form'>
+
+    </div>
+    <div className='avion'>
+
+    </div>
+
+    
+    </>
+  )
+}
+
+export default Home
