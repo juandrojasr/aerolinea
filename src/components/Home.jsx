8578825049f027bb8bc51f59087ac3573c9b3931
juandrojasr <juandrojasr97@gmail.com>
2023-02-02 17:33:28 -0500

    feat: img and p

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import img1 from '../assets/images/avion.jpg'
 
 const Home = () => {
   return (
@@ -7,6 +8,13 @@ const Home = () => {
 
     </div>
     <div className='avion'>
+        <img src={img1} />
+
+    </div>
+    <div className='pagoseguro'>
+        <p>Tarjeta de credito, tarjeta de debito y pago electronico</p>
+    </div>
+    <div className='servicios'>
 
     </div>
 
