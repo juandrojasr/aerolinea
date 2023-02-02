0f775ffea756ee227a1f3aff12427ce6e955e644
juandrojasr <juandrojasr97@gmail.com>
2023-02-02 18:05:46 -0500

    feat: pagoseguro

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,26 +1,40 @@
 import React from 'react'
 import img1 from '../assets/images/avion.jpg'
+import img2 from '../assets/images/tarjetas.png'
+import img3 from '../assets/images/efectivo.png'
 
 const Home = () => {
-  return (
-    <>
-    <div className='form'>
+    return (
+        <>
+            <div className='form'>
 
-    </div>
-    <div className='avion'>
-        <img src={img1} />
+            </div>
+            <div className='avion'>
+                <img src={img1} />
 
-    </div>
-    <div className='pagoseguro'>
-        <p>Tarjeta de credito, tarjeta de debito y pago electronico</p>
-    </div>
-    <div className='servicios'>
+            </div>
+            <div className='pagoseguro'>
+                <div className='tarjeta'>
+                    <p>
+                        Tarjeta de credito, tarjeta de debito y pago electronico
+                    </p>
+                    <img src={img2} alt="banner tarjetas" />
 
-    </div>
+                </div>
+                <div className='efectivo'>
+                    <p>
+                        Efectivo en cualquiera de las sucrusales participantes.
+                    </p>
+                    <img src={img3} alt="efectivo" />
+                </div>
+            </div>
+            <div className='servicios'>
 
-    
-    </>
-  )
+            </div>
+
+
+        </>
+    )
 }
 
 export default Home
